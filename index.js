25328a0ed9eb969b19524fbf4ea563be8da9efd9
vishal <mishravishal958@gmail.com>
2023-03-02 14:33:50 +0530

    Added tstamp

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-// index.js
-// where your node app starts
+var moment = require('moment');
 
-// init project
 var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
+const { type } = require('express/lib/response');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
@@ -24,9 +23,64 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date", function (req, res) {
+
+   /*if (moment.utc(date, 'YYYY-M-D', true).isValid() || moment.utc(date, 'YYYY-M-D', true).isValid()) {
+    res.json({
+      unix: new Date(date).getTime(),
+      utc: new Date(date).toUTCString()
+    });
+  }
+  else if (new Date(date)!="Invalid Date") {
+    res.json({
+      unix: new Date(date).getTime(),
+      utc: new Date(date).toUTCString()
+    });
+  }
+  else if (!isNaN(date)) {
+    res.json({
+      unix: new Date(parseInt(date)).getTime(),
+      utc: new Date(parseInt(date)).toUTCString()
+    });
+  }
+  else {
+    res.send({"error":"Invalid Date"});
+  }*/
+  const dateString=req.params.date;
+  let date;
+  if(!dateString){
+    date=new Date();
+  }
+  else{
+    if(!isNaN(dateString)){
+      date=new Date(parseInt(dateString));
+    }
+    else{
+      date=new Date(dateString);
+    }
+  }
+  if(date.toString()=='Invalid Date'){
+    res.json({error: date.toString()});
+  }
+  else{
+    res.json({unix: date.getTime(),utc:date.toUTCString()});
+  }
+  
+});
+
+
+app.get("/api", (req, res)=>{
+  let date = new Date();
+  
+  return res.json({
+    'unix': date.getTime(), 
+    'utc': date.toUTCString()
+  });  
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(8080, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
